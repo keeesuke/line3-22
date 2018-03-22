11d895219ab918a91cf22b806d62a5be424f59ee
keeesuke <keisuke@testnoMacBook-Pro.local>
2018-03-22 20:42:43 +0900

    calendar

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -1,4 +1,4 @@
-package codecheck;
+import java.util.Calendar;
 
 public class App {
 	public static void main(String[] args) {
@@ -6,5 +6,12 @@ public class App {
 			String output = String.format("argv[%s]: %s", i, args[i]);
 			System.out.println(output);
 		}
+
+    Calendar cal = Calendar.getInstance();
+
+    int month = cal.get(Calendar.MONTH) + 1;
+    int day = cal.get(Calendar.DATE);
+
+    System.out.println("今日は" + month + "月" + day + "です");
 	}
 }
